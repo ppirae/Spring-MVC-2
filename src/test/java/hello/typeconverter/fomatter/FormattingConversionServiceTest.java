3e71c0dd95f3d5a3e6b8a8ac48a852a522aecc26
Hyeongrae Jo <76774733+ppirae@users.noreply.github.com>
2022-06-25 20:36:23 +0900

    스프링 기본 formatter

--- src/test/java/hello/typeconverter/fomatter/FormattingConversionServiceTest.java
+++ src/test/java/hello/typeconverter/fomatter/FormattingConversionServiceTest.java
@@ -7,6 +7,8 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.format.support.DefaultFormattingConversionService;
 
+import static org.assertj.core.api.Assertions.*;
+
 public class FormattingConversionServiceTest {
 
     @Test
@@ -20,6 +22,9 @@ public class FormattingConversionServiceTest {
 
         //컨버터 사용
         IpPort ipPort = conversionService.convert("127.0.0.1:8080", IpPort.class);
-        Assertions
+        assertThat(ipPort).isEqualTo(new IpPort("127.0.0.1", 8080));
+        //포맷터 사용
+        assertThat(conversionService.convert(1000, String.class)).isEqualTo("1,000");
+        assertThat(conversionService.convert("1,000", Long.class)).isEqualTo(1000);
     }
 }
